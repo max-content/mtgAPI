57c2495d04e70439d6dbcb160117c7c5d51d17e1
Max Salzman <mnsalz91@gmail.com>
2023-03-28 10:02:33 -0400

    consolidate newclient & empty client app to client

--- reactVersion/client/src/components/Game.jsx
+++ reactVersion/client/src/components/Game.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { socket } from '.././socket';
+import { socket } from '../socket';
 
 // import io from 'socket.io-client';
 // import dotenv from 'dotenv';
